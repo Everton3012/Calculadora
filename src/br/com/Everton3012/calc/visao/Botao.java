f1609a6916a908f01d4b4c2442aae53fa267e5df
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2026-08-24 21:43:34 -0300

    adiciona construtor à classe Botao e implementa layout do Teclado com botões

--- src/br/com/Everton3012/calc/visao/Botao.java
+++ src/br/com/Everton3012/calc/visao/Botao.java
@@ -1,7 +1,18 @@
 package br.com.Everton3012.calc.visao;
 
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 
 public class Botao extends JButton {
 
+    public Botao(String texto, Color cor) {
+        setText(texto);
+        setBackground(cor);
+        setOpaque(true);
+        setFont(new Font("Arial", Font.PLAIN, 25));
+        setForeground(Color.WHITE);
+        setBorder(BorderFactory.createLineBorder(Color.BLACK));
+    }
 }
