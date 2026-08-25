f8cd721159b89a58a51563afaed5032f20324e10
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2026-08-25 15:15:50 -0300

    adiciona enum TipoComando à classe Memoria e implementa método para detectar tipo de comando

--- src/br/com/Everton3012/calc/modelo/Memoria.java
+++ src/br/com/Everton3012/calc/modelo/Memoria.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 public class Memoria {
 
+    private enum TipoComando {
+        ZERAR, NUMERO, DIV, MULT, SUB, SOMA, IGUAL, VIRGULA
+    }
+
     private static Memoria instancia = new Memoria();
 
     private final List<MemoriaObservador> observadores = new java.util.ArrayList<>();
@@ -26,11 +30,43 @@ public class Memoria {
     }
 
     public void processarComando(String texto) {
+
+        TipoComando tipoComando = detectarTipoComando(texto);
+
         if (texto.equalsIgnoreCase("AC")) {
             textoAtual = "";
         } else {
             textoAtual += texto;
         }
         observadores.forEach(o -> o.valorAlterado(getTextoAtual()));
+    }
+
+    private TipoComando detectarTipoComando(String texto) {
+        if (textoAtual.isEmpty() && texto.equalsIgnoreCase("0")) {
+            return null;
+        }
+
+        try {
+            Integer.parseInt(texto);
+            return TipoComando.NUMERO;
+        } catch (NumberFormatException e) {
+            if (texto.equalsIgnoreCase("AC")) {
+                return TipoComando.ZERAR;
+            } else if (texto.equalsIgnoreCase("/")) {
+                return TipoComando.DIV;
+            } else if (texto.equalsIgnoreCase("*")) {
+                return TipoComando.MULT;
+            } else if (texto.equalsIgnoreCase("-")) {
+                return TipoComando.SUB;
+            } else if (texto.equalsIgnoreCase("+")) {
+                return TipoComando.SOMA;
+            } else if (texto.equalsIgnoreCase("=")) {
+                return TipoComando.IGUAL;
+            } else if (texto.equalsIgnoreCase(",")) {
+                return TipoComando.VIRGULA;
+            }
+        }
+
+        return null;
     }
 }
